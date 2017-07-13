82d4b1009224355a31bd0dc4063aa1ccda395a9a
unknown <>
2017-07-13 09:59:16 +0800

    添加项目文件。

---
+++ 文件自动备份系统/BackupCore.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace 自动备份系统
+{
+    class BackupCore
+    {
+        //用于读取配置文件
+        Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+    
+
+        public void Backup(string name)
+        {
+            //获取黑白名单目录
+            string tempWhiteListViewItems;
+            tempWhiteListViewItems = cfa.AppSettings.Settings[name + "_White"] != null ? cfa.AppSettings.Settings[name + "_White"].Value : "";
+            foreach (var i in tempWhiteListViewItems.Split(new string[] { "??" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                whiteDirectories.Add(i);
+            }
+            string tempBlackListViewItems;
+            tempBlackListViewItems = cfa.AppSettings.Settings[name + "_Black"] != null ? cfa.AppSettings.Settings[name + "_Black"].Value : "";
+            foreach (var i in tempBlackListViewItems.Split(new string[] { "??" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+               blackDirectories.Add(i);
+            }
+            //挑出其中的文件，其余的列举文件
+            foreach (var i in whiteDirectories)
+            {
+                if (new FileInfo(i).Attributes == FileAttributes.Directory)
+                {
+                    listFiles(i);
+                }
+                else
+                {
+                    fileName.Add(new FileInfo(i).Name);
+                }
+            }
+            targetDirectory = cfa.AppSettings.Settings[name + "_TargetDirectory"].Value;
+            //列举目标目录文件
+            listBackupedFiles(targetDirectory);
+            //列举差异项
+            listDiferrences();
+            //将不同的部分复制到目标文件夹
+            moveDiferrences();
+            //foreach (var i in fileName)
+            //{
+            //    Debug.WriteLine(i);
+            //}
+        }
+
+
+
+        string targetDirectory; 
+        //黑白名单的目录
+        List<string> whiteDirectories = new List<string>();
+        List<string> blackDirectories = new List<string>();
+        //源目录文件去掉相同地址后的文件名、文件全名、修改时间和大小
+        List<string> fileName = new List<string>();
+        List<string> fullFileName = new List<string>();
+        List<DateTime> fileLastWriteTime = new List<DateTime>();
+        List<long> fileLength = new List<long>();
+    //备份过的文件的文件名、修改时间和大小
+        List<string> backupedFileName = new List<string>();
+        List<DateTime> backupedFileLastWriteTime = new List<DateTime>();
+        List<long> backupedFileLength = new List<long>();
+        /// <summary>
+        /// 列举源目录文件
+        /// </summary>
+        /// <param name="path"></param>
+        private void listFiles(string path)
+        {
+
+           // int n = 0;
+            foreach (var i in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                //用于跳出两层循环，代替goto
+                bool needBackup = true;
+                foreach (var j in blackDirectories)
+                {
+                    if (i.StartsWith(j))//如果是黑名单的文件
+                    {
+                        needBackup = false;
+                    }
+                }
+                if (needBackup)
+                {
+                  //  n++;
+                    fullFileName.Add(i);//文件全名
+                    fileName.Add(i.Replace(path,""));//去除相同目录的文件名
+                    FileInfo fif = new FileInfo(i);
+                    fileLastWriteTime.Add(fif.LastWriteTimeUtc);//文件修改时间
+                    fileLength.Add(fif.Length);//文件大小
+                }
+            }
+        }
+        /// <summary>
+        /// 列举目标目录里面已经备份过的文件
+        /// </summary>
+        /// <param name="path"></param>
+        private void listBackupedFiles(string path)
+        {
+
+            //int n = 0;
+            foreach (var i in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                //n++;
+                backupedFileName.Add(i.Replace(path, ""));
+                FileInfo fif = new FileInfo(i);
+                backupedFileLastWriteTime.Add(fif.LastWriteTimeUtc);
+                backupedFileLength.Add(fif.Length);
+            }
+        }
+
+        //List<int> differentFilesIndex = new List<int>();
+        List<int> sameFilesIndex = new List<int>();
+        /// <summary>
+        /// 列举源目录和目标目录不同的文件，并且把相同的但是修改时间不同的文件改名用于备份
+        /// </summary>
+        private void listDiferrences()
+        {
+            //列举每一个源目录里的文件，寻找目标目录是否有相同的文件
+           for(int i=0;i<fileName.Count;i++)
+            {
+                for(int j=0;j<backupedFileName.Count;j++)//列举目标目录文件
+                {
+                    FileInfo targetFile = new FileInfo(targetDirectory + "\\" + fullFileName[i].Replace(fileName[i], "").Replace(":", "#C#").Replace("\\", "#S#") + fileName[i]);
+                    //                                               目标目录                             源目录                替换掉不同的部分              把冒号替换             把斜杠替换    加上名字
+                    if (backupedFileName[j].EndsWith(fileName[i]))//如果找到相同文件名的文件
+                    {
+                        if (fileLastWriteTime[i] == backupedFileLastWriteTime[j])//如果修改时间相同
+                        {
+                            if(fileLength[i]==backupedFileLength[j])//如果文件大小相同
+                            {
+                                sameFilesIndex.Add(i);
+                                //文件名、文件大小和文件修改时间全部相同，几乎可以证明两个文件相同
+                            }
+                            else
+                            {
+                                //修改时间相同但是大小不同，应该说是一件比较蹊跷的事，但是还是考虑一下
+                                targetFile.MoveTo(targetFile.FullName.Replace(targetFile.Extension,"")+"??"+ targetFile.Extension);
+                                //differentFilesIndex.Add(i);
+                            }
+                        }
+                        else
+                        {
+                            //如果文件名一样但是修改时间变新了，说明后来修改过文件
+                            //此时要把原来的文件加上时间标签重命名
+                           // FileInfo targetFile = new FileInfo(targetDirectory + "\\" + fullFileName[i].Replace(fileName[i], "").Replace(":", "#C#").Replace("\\", "#S#") + fileName[i]);
+                            targetFile.MoveTo(targetFile.FullName.Replace(targetFile.Extension, "") + targetFile.LastWriteTimeUtc.ToFileTimeUtc() + targetFile.Extension);
+                            //differentFilesIndex.Add(i);
+                        }
+                            //backupedFileName.RemoveAt(j);
+                    }
+                    continue;
+                }
+                
+            }
+        }
+        /// <summary>
+        /// 备份差异项
+        /// </summary>
+        private void moveDiferrences()
+        {
+            int skipFile = 0;
+           for(int i=0;i<fileName.Count;i++)//循环每一个源文件
+            {
+                if(sameFilesIndex.Contains(i))//如果文件索引出现在了相同文件的List上
+                {
+                    skipFile++;
+                    continue;//跳过备份
+                }
+                FileInfo targetFile = new FileInfo(targetDirectory+"\\" +fullFileName[i].Replace(fileName[i],"").Replace(":","#C#").Replace("\\","#S#")+ fileName[i]);
+                if(!targetFile.Directory.Exists)
+                {
+                    //如果目标文件的目录不存在的话就创建一个，否则会报异常
+                    targetFile.Directory.Create();
+                }
+                File.Copy(fullFileName[i], targetFile.FullName);
+            }
+
+        }
+    }
+}
---
+++ 文件自动备份系统/TaskSettings.xaml.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using System.Configuration;
+
+
+
+namespace 自动备份系统
+{
+    /// <summary>
+    /// TaskSettings.xaml 的交互逻辑
+    /// </summary>
+    public partial class TaskSettings : Window
+    {
+
+        string name;
+        public TaskSettings(string taskName)
+        {
+            InitializeComponent();
+            name = taskName;
+        }
+        #region 白名单
+        private void NewWhiteDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            if(Directory.Exists(txtNewWhiteDirectory.Text))
+            {
+                lvwWhite.Items.Add(txtNewWhiteDirectory.Text);
+                txtNewWhiteDirectory.Text = "输入地址或者拖放目录到列表框";
+            }
+            else
+            {
+                MessageBox.Show("请检查输入的地址是否正确！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void DeleteWhiteDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            int index = lvwWhite.SelectedIndex;
+            if(index!=-1)
+            {
+                lvwWhite.Items.RemoveAt(index);
+                if(lvwWhite.Items.Count!=0)
+                {
+                    lvwWhite.SelectedIndex = (index == lvwWhite.Items.Count ? index - 1 : index);
+                    //如果刚刚选择的是最后一个了，那么选择前面一个，否则选择后面一个
+                }
+            }
+        }
+
+        private void LvwWhiteDragEnterEventHandler(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+                txtNewWhiteDirectory.Text = "拖放到列表框上方加入白名单";
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void LvwWhiteDropEventHandler(object sender, DragEventArgs e)
+        {
+            txtNewWhiteDirectory.Text = "输入地址或者拖放目录到列表框";
+            try
+            {
+                foreach (var item in (string[])e.Data.GetData(DataFormats.FileDrop, false))
+                {
+                    lvwWhite.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请检查是否拖入了不可引用的目录！","错误",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        private void LvwWhiteDragLeaveEventHandler(object sender, DragEventArgs e)
+        {
+            txtNewWhiteDirectory.Text = "输入地址或者拖放目录到列表框";
+        }
+
+        private void SelectWhiteDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog() { Description = "请选择需要备份的目录", ShowNewFolderButton = true };
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                lvwWhite.Items.Add(fbd.SelectedPath);
+            }
+        }
+        #endregion
+
+        #region 黑名单
+        private void NewBlackDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            if (Directory.Exists(txtNewBlackDirectory.Text))
+            {
+                lvwBlack.Items.Add(txtNewBlackDirectory.Text);
+                txtNewBlackDirectory.Text = "输入地址或者拖放目录到列表框";
+            }
+            else
+            {
+                MessageBox.Show("请检查输入的地址是否正确！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void DeleteBlackDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            int index = lvwBlack.SelectedIndex;
+            if (index != -1)
+            {
+                lvwBlack.Items.RemoveAt(index);
+                if (lvwBlack.Items.Count != 0)
+                {
+                    lvwBlack.SelectedIndex = (index == lvwBlack.Items.Count ? index - 1 : index);
+                    //如果刚刚选择的是最后一个了，那么选择前面一个，否则选择后面一个
+                }
+            }
+        }
+
+        private void LvwBlackDragEnterEventHandler(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+                txtNewBlackDirectory.Text = "拖放到列表框上方加入白名单";
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void LvwBlackDropEventHandler(object sender, DragEventArgs e)
+        {
+            txtNewBlackDirectory.Text = "输入地址或者拖放目录到列表框";
+            try
+            {
+                foreach (var item in (string[])e.Data.GetData(DataFormats.FileDrop, false))
+                {
+                    lvwBlack.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请检查是否拖入了不可引用的目录！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LvwBlackDragLeaveEventHandler(object sender, DragEventArgs e)
+        {
+            txtNewBlackDirectory.Text = "输入地址或者拖放目录到列表框";
+        }
+
+        private void SelectBlackDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog() { Description = "请选择需要排除的目录", ShowNewFolderButton = true };
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                lvwBlack.Items.Add(fbd.SelectedPath);
+            }
+        }
+        #endregion
+
+        private void SelectTargetDirectoryButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog() { Description="请选择希望把文件备份到的目标目录",ShowNewFolderButton=true};
+            
+            if (fbd.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                txtTargetDirectory.Text = fbd.SelectedPath;
+            }
+        }
+        Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        private void OKButtonClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            string white = "";
+            foreach (var i in lvwWhite.Items)
+            {
+                white += i.ToString()+"??";
+            }
+
+            ChangeAppSettings("White", white);
+
+            string black = "";
+            foreach (var i in lvwBlack.Items)
+            {
+                black += i.ToString() + "??";
+            }
+
+            ChangeAppSettings("Black", black);
+
+            ChangeAppSettings("TargetDirectory", txtTargetDirectory.Text);
+
+            int multiplier = 1;
+           switch(cboTimeUnit.SelectedIndex)
+            {
+                case 1: multiplier = 60;
+                    break;
+                case 2:multiplier = 3600;
+                    break;
+                case 3:multiplier = 86400;
+                    break;
+                case 4:multiplier = 604800;
+                    break;
+            }
+            try { ChangeAppSettings("Interval", (int.Parse(txtInterval.Text) * multiplier).ToString()); }
+            catch(Exception)
+            {
+                MessageBox.Show("输入的时间有误！");
+            }
+
+            cfa.Save();
+        }
+
+        private void ChangeAppSettings(string key,string targetValue)
+        {
+            key = txtName.Text+"_"+key;
+            if (cfa.AppSettings.Settings[key] == null)
+            {
+                cfa.AppSettings.Settings.Add(new KeyValueConfigurationElement(key, targetValue));
+            }
+            else
+            {
+                cfa.AppSettings.Settings[key].Value = targetValue;
+            }
+        }
+
+        private void WindowLoadedEventHandler(object sender, RoutedEventArgs e)
+        {
+            string tempWhiteListViewItems;
+            tempWhiteListViewItems = cfa.AppSettings.Settings[name + "_White"] != null ? cfa.AppSettings.Settings[name + "_White"].Value : "";
+            foreach (var i in tempWhiteListViewItems.Split(new string[] { "??" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                lvwWhite.Items.Add(i);
+            }
+            string tempBlackListViewItems;
+            tempBlackListViewItems = cfa.AppSettings.Settings[name + "_Black"] != null ? cfa.AppSettings.Settings[name + "_Black"].Value : "";
+            foreach (var i in tempBlackListViewItems.Split(new string[] { "??" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                lvwBlack.Items.Add(i);
+            }
+
+            txtTargetDirectory.Text = cfa.AppSettings.Settings[name + "_TargetDirectory"] != null ? cfa.AppSettings.Settings[name + "_TargetDirectory"].Value : "";
+            txtInterval.Text = cfa.AppSettings.Settings[name + "_Interval"] != null ? cfa.AppSettings.Settings[name + "_Interval"].Value : "60";
+            txtName.Text = name;
+
+        }
+    }
+}
+    
